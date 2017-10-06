897da0611e6da95f0be004f781fbbe144f5c37a3
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 13:52:02 +0100

    Added unit test for rook movement

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Square from '../../../src/engine/square';
+
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +8,14 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        // return new Array(0);
+    let moves = []
+    let location = board.findPiece(this);
+    console.log(location);
+    moves = [ Square.at(1, 0), Square.at(1, 1), Square.at(1, 3), 
+        Square.at(1, 4), Square.at(1, 5), Square.at(1, 6), Square.at(1, 7),
+        Square.at(0, 2), Square.at(2, 2), Square.at(3, 2), Square.at(4, 2), 
+        Square.at(5, 2), Square.at(6, 2), Square.at(7, 2)];
+        return moves
     }
 }
